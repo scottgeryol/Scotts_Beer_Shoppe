6478d6cd9412a6bd96c88e0d1567065beb30e0b1
Scott Geryol <sgeryol@gmail.com>
2020-03-07 18:42:17 -0600

    Created through Show

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,21 @@
 class Api::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render "index.json.jb"
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      abv: params[:abv],
+      style: params[:style],
+    )
+    @product.save
+    render "show.json.jb"
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.json.jb"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+beer = Product.create({ name: "Half Acre - Tome", abv: 5.5, style: "Hazy Pale Ale" })
+beer = Product.create({ name: "Toppling Goliath - Psuedo Sue", abv: 6, style: "DDH Pale Ale" })
+beer = Product.create({ name: "Alarmist - The Last Gary", abv: 7, style: "NE IPA" })
